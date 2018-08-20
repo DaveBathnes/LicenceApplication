fa51a3fa725d00c8a5a03498957e73222039d8b6
Dave <DaveBathnes@users.noreply.github.com>
2018-08-20 23:18:35 +0100

    Getting basics finished

---
+++ LicenceApplication/LicenceApplication.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace LicenceApplication
+{
+    /// <summary>
+    /// The Licence Types
+    /// </summary>
+    enum LicenceType { New, Renewal };
+
+    /// <summary>
+    /// Our Main Licence Application Class. Holds Methods for a Licence Application
+    /// </summary>
+    class LicenceApplication
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="licenceType"></param>
+        /// <param name="dvla"></param>
+        /// <param name="licenceTerm"></param>
+        /// <param name="dateOfBirth"></param>
+        /// <returns></returns>
+        public static LicenceEligibilityResponse CheckLicenceEligibility(LicenceType licenceType, bool dvla, int licenceTerm, DateTime dateOfBirth)
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+            var configuration = builder.Build();
+
+            // Get some initial configuration settings
+            int minimumAge = int.Parse(configuration["minimum_age"]);
+
+            LicenceEligibilityResponse response = new LicenceEligibilityResponse();
+            var messages = new List<string>();
+            var costs = new List<string>();
+            double total = 0.00;
+            double discount = 0.00;
+            response.success = true;
+
+            // 1. First validate the date of birth
+            int age = DateTime.Now.Year - dateOfBirth.Year;
+            if (DateTime.Now.DayOfYear < dateOfBirth.DayOfYear) age = age - 1;
+            if (age <= minimumAge)
+            {
+                response.success = false;
+                messages.Add(String.Format("Licence applicants must be {0} years or older.", minimumAge.ToString()));
+            }
+
+            // 2. Then validate the DVLA licence
+            if (!dvla)
+            {
+                response.success = false;
+                messages.Add(String.Format("Licence applicants must hold a full DVLA licence."));
+            }
+
+            // 
+
+
+
+
+
+            // Apply any discount
+            total = total - (total * discount);
+
+
+
+            response.messages = messages;
+            response.costs = costs;
+            response.total = total;
+            return response;
+        }
+    }
+
+    /// <summary>
+    /// Class to hold a response object returned by a licence eligibility request.
+    /// </summary>
+    class LicenceEligibilityResponse
+    {
+        public bool success { get; set; }
+        public List<string> messages { get; set; }
+        public List<string> costs { get; set; }
+        public double total { get; set; }
+    }
+
+}
---
+++ LicenceApplication/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+using Microsoft.Extensions.Configuration;
+
+namespace LicenceApplication
+{
+    class Program
+    {
+        /// <summary>
+        /// The main entry point of the application. Takes in user input for the licence application
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            // Application Configuration
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+            var configuration = builder.Build();
+
+            // Inputs to eligibility check. Licence Type, Term, 
+            LicenceType type = LicenceType.New;
+            int term = 1;
+            bool dvla = false;
+            DateTime dob;
+
+            // Licence Type Input
+            ConsoleKey renewal_response;
+            do
+            {
+                Console.Write("Is this a renewal of an existing licence? [y/n] ");
+                renewal_response = Console.ReadKey(false).Key;
+                if (renewal_response != ConsoleKey.Enter) Console.WriteLine();
+            } while (renewal_response != ConsoleKey.Y && renewal_response != ConsoleKey.N);
+
+            // Renewal Term Input
+            if (renewal_response == ConsoleKey.Y)
+            {
+                ConsoleKeyInfo termResponse;
+                do
+                {
+                    Console.Write("How many years would you like to renew for? [1/2/3] ");
+                    termResponse = Console.ReadKey(false);
+                    if (termResponse.Key != ConsoleKey.Enter) Console.WriteLine();
+                } while (termResponse.Key != ConsoleKey.D1 && termResponse.Key != ConsoleKey.D2 && termResponse.Key != ConsoleKey.D3);
+                term = (int)char.GetNumericValue(termResponse.KeyChar);
+            }
+
+            // DVLA Licence Input
+            ConsoleKey dvla_held;
+            do
+            {
+                Console.Write("Do you hold a full DVLA driving licence? [y/n] ");
+                dvla_held = Console.ReadKey(false).Key;
+                if (dvla_held != ConsoleKey.Enter)
+                {
+                    Console.WriteLine();
+                }
+            } while (dvla_held != ConsoleKey.Y && dvla_held != ConsoleKey.N);
+            dvla = (dvla_held == ConsoleKey.Y ? true : false);
+
+            // DOB Input
+            string dob_response;
+            string[] formats = { "dd/MM/yyyy" };
+            do
+            {
+                Console.Write("What is your date of birth? [DD/MM/YYYY] ");
+                dob_response = Console.ReadLine();
+            } while (!DateTime.TryParseExact(dob_response, formats, CultureInfo.InvariantCulture, DateTimeStyles.None, out dob));
+
+            var response = LicenceApplication.CheckLicenceEligibility(type, dvla, term, dob);
+
+            // Print out the response - different styles if success or rejection
+            if (response.success)
+            {
+                Console.Write("Yes");
+            } else
+            {
+                Console.Write("No!");
+            }
+
+            // Keep application alive 
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
